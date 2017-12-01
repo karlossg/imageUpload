882b2faf2d334cd0cc7dd8ea17147bdb5e9bb40a
karlossg <karol.gratus@gmail.com>
2017-12-01 23:49:03 +0100

    working on it

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,25 +1,30 @@
 const fs = require('fs');
 const formidable = require('formidable');
 
-let uploadFileName = ''
+// let uploadFileName = '';
 
 exports.upload = (request, response) => {
   console.log("Rozpoczynam obsługę żądania upload.");
   const form = new formidable.IncomingForm();
   form.parse(request, (error, fields, files) => {
-    
-      if (fields.title === '') {
-        uploadFileName = files.upload.name;
-      } else {
-        uploadFileName = `${fields.title}.${files.upload.name.slice(-3)}`;
-      }
       
-      fs.renameSync(files.upload.path, uploadFileName);
-      fs.readFile('templates/upload.html', (err, html) => {
+    const uploadFileName = fields.title ? `${fields.title}.${files.upload.extension}` : files.upload.name
+              console.log(files.upload)
+    fs.renameSync(files.upload.path, `${uploadFileName}`);
+    fs.readFile('templates/upload.html', (err, html) => {
         response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
         response.write(html);
         response.end();
     });
+
+    exports.show = (request, response) => {
+      fs.readFile(uploadFileName, "binary", (error, file) => {
+          response.writeHead(200, {"Content-Type": "image/png"});
+          response.write(file, "binary");
+          response.end();
+      });
+    }
+
  });
 }
 
@@ -32,13 +37,6 @@ exports.welcome = (request, response) => {
   });
 }
 
-exports.show = (request, response) => {
-  fs.readFile(uploadFileName, "binary", (error, file) => {
-      response.writeHead(200, {"Content-Type": "image/png"});
-      response.write(file, "binary");
-      response.end();
-  });
-}
 
 exports.error = (request, response) => {
   console.log("Nie wiem co robić.");
