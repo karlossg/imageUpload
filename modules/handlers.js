b84db9575c3a9fc2fd1da4513c253fbf0fec8ca2
Karol <karol.gratus@gmail.com>
2017-12-01 11:14:38 +0100

    opload

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,13 +1,25 @@
+const fs = require('fs');
+const formidable = require('formidable');
+
 exports.upload = (request, response) => {
   console.log("Rozpoczynam obsługę żądania upload.");
-  response.write("Rozpoczynam upload!");
-  response.end();
+  const form = new formidable.IncomingForm();
+  form.parse(request, (error, fields, files) => {
+      fs.renameSync(files.upload.path, "test.png");
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.write("received image:<br/>");
+      response.write("<img src='/show' />");
+      response.end();
+  });
 }
 
 exports.welcome = (request, response) => {
   console.log("Rozpoczynam obsługę żądania welcome.");
-  response.write("Witaj na stronie startowej!");
-  response.end();
+  fs.readFile('templates/start.html', (err, html) => {
+      response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+      response.write(html);
+      response.end();
+  });
 }
 
 exports.error = (request, response) => {
