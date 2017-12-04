406aabdace354cde78797cdc4f52a98eedde9964
Karol <karol.gratus@gmail.com>
2017-12-04 13:58:23 +0100

    css working

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const formidable = require('formidable');
 
-
-
 exports.upload = (request, response) => {
   console.log("Rozpoczynam obsługę żądania upload.");
   const form = new formidable.IncomingForm();
@@ -10,12 +8,13 @@ exports.upload = (request, response) => {
       
     const uploadFileName = fields.title ? `${fields.title}.${files.upload.name.slice(-3)}` : files.upload.name
               
-    fs.renameSync(files.upload.path, `${uploadFileName}`);
+    fs.rename(files.upload.path, `${uploadFileName}`);
     fs.readFile('templates/upload.html', (err, html) => {
         response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
         response.write(html);
         response.end();
     });
+    
 
     exports.show = (request, response) => {
       fs.readFile(uploadFileName, "binary", (error, file) => {
@@ -26,6 +25,8 @@ exports.upload = (request, response) => {
     }
 
  });
+
+ 
 }
 
 exports.welcome = (request, response) => {
@@ -37,9 +38,17 @@ exports.welcome = (request, response) => {
   });
 }
 
+exports.style = (request, response) => {
+  fs.readFile('style.css', (err, css) => {
+    response.writeHead(200, {"Content-Type": "text/css"});
+    response.write(css);
+    response.end();
+  });
+}
 
 exports.error = (request, response) => {
   console.log("Nie wiem co robić.");
   response.write("404 :(");
   response.end();
 }
+
--- modules/server.js
+++ modules/server.js
@@ -20,6 +20,9 @@ function start() {
         case '/show':
             handlers.show(request, response);
             break;
+        case "/style.css" :
+            handlers.style(request, response);
+            break;
         default:
             handlers.error(request, response);
     }
